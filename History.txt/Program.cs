0d86708b00c4a3d923719059dfe76cd34725ee8d
Luis A. Villalba <luisvillalba_12@hotmail.com>
2023-08-17 21:32:32 -0500

    Cambios al archivo de historia para editar las primeras lineas de codigo

--- History.txt/Program.cs
+++ History.txt/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+/ Esta  es la historia de Luis A. Villalba
+
+    Luis A. Villalba tiene 42 años y nacio en Colombia,
+    viviendo en todo el mundo
+
+    Hoy hablaremos de su historia.}
+}
